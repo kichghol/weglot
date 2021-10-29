d2c55c806b737685a34863fb3991c29f5a010a2d
kichghol <nouraibriz@outlook.fr>
2021-10-29 17:23:47 +0200

    done

--- src/index.js
+++ src/index.js
@@ -1,71 +1,64 @@
 const fs = require("fs");
 const moment = require("moment")
-
-const content = fs.readFileSync(process.cwd() + "/data/input1.txt");
-
+const content = fs.readFileSync(process.cwd() + "/data/input5.txt");
 const schedules = content.toString().split("\r\n")
 
 function freeSchedule(schedules){
-	
 	schedules.sort()
-	// console.log(schedules)
 	return findSchedule(schedules)
 }
 
-
 function findSchedule(schedules) {
-    const schedulesObject = {}
-		// boucle pour trier par jour 
+  const schedulesObject = {}
+		
     for (const schedule of schedules) {
         const key = schedule.charAt(0)
         const value = schedule.substring(2)
-        if (key in schedulesObject) {
+
+			if (key in schedulesObject) {
             schedulesObject[key].push(value)
             
-        }else{
+			}else{
         schedulesObject[key] = [value]
-				}
+			}
     }
-		// 
+
     for (const key in schedulesObject) {
-        let [hourBegining,hourEnding] = parseHourInADay(schedulesObject[key])
-        if (hourBegining) {
-						// console.log(momentDate.format("YYYY-MM-DD hh:mm:ss A Z"));
-					// console.log(key,hourBegining.format("HH:mm"),"-",hourEnding.format("HH:mm") );
-					return	`${key} ${hourBegining.format("HH:mm")} - ${hourEnding.format("HH:mm")}` 		
-        }
+			let [hourBegining,hourEnding] = parseHourInADay(schedulesObject[key])
+
+			if (hourBegining) {
+				return	`${key} ${hourBegining.format("HH:mm")}-${hourEnding.format("HH:mm")}` 		
+			}
     }
 }
-
 
 function parseHourInADay(schedules) {
     let hourBegining= moment("08:00","HH:mm")
 		let hourEnding=moment("08:59","HH:mm") 
-	// pour chaque creneau on sépare le debut et la fin en fonction du '-'
+
     for (const schedule of schedules) {
         let [scheduleStart, scheduleEnd] = schedule.split("-")
-				// mmoment 'module pour convertir '
 				scheduleStart = moment(scheduleStart,"HH:mm") 
 				scheduleEnd = moment(scheduleEnd,"HH:mm")
-				// on verifie notre heure de debut et de fin est en dehors du creneau actuel 
-        if(isTaken(hourBegining, scheduleStart,scheduleEnd) || isTaken(hourEnding ,scheduleStart, scheduleEnd))  {
-					hourBegining = scheduleEnd.add(1,'minutes')
-					hourEnding =  moment(hourBegining).add(59,'minutes')
-        }
+
+			if(isTaken(hourBegining, scheduleStart,scheduleEnd) || isTaken(hourEnding ,scheduleStart, scheduleEnd))  {
+				hourBegining = scheduleEnd.add(1,'minute')
+				hourEnding =  moment(hourBegining).add(59,'minutes')	
+			}
     }
 
     if (hourEnding.isBefore(moment ("17:59","HH:mm"))){
 			return [hourBegining,hourEnding]
 		}
+
     return [null,null]
 }
 
 function isTaken(hour, scheduleStart,scheduleEnd) {
-	// isBefore fonction du module moment qui sert à verifier 
-    return (hour.isBefore(scheduleEnd) && scheduleStart.isBefore(hour)  )
+    return (hour.isSameOrBefore(scheduleEnd) && scheduleStart.isBefore(hour)  )
 }
 
 
-// freeSchedule(schedules);
+// console.log(freeSchedule(schedules));
 
 module.exports = freeSchedule ;
--- test/index.spec.js
+++ test/index.spec.js
@@ -2,24 +2,24 @@ const freeSchedule = require('../src/index.js');
 const fs = require("fs");
 const testing = './data/'
 
-// const content = fs.readFileSync(process.cwd() + "/data/input1.txt");
-
-
-it('adds 1 + 2 to equal 3', () => {
+test('test schedule', () => {
     fs.readdir(testing ,(error,files)=> {
 			if(error){
 				
 				console.log(error);
 			}
-		const	inPutFiles= files.slice(5)
+		const	inPutFiles= files.slice(0,5)
 		const	outPutFiles= files.slice(-5)
-			for (const i= 0; i< 5 ; i++) {
+
+			for (let i= 0; i< 5 ; i++) {
 				const file = inPutFiles[i]
 				const outPutFile = fs.readFileSync(process.cwd() + "/data/"+outPutFiles[i]);
 				const outPut = outPutFile.toString().split("\r\n")
 				const content = fs.readFileSync(process.cwd() + "/data/"+file);
 				const schedules = content.toString().split("\r\n")
+
 				expect(freeSchedule(schedules)).toBe(outPut[0])
+				
 			};
 		});
   });
